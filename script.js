4564dcf18d3acd80355651be80205e85b2e5a314
asst3rix_WC <185900998+asst3rix@users.noreply.github.com>
2026-09-16 16:57:02 +0200

    Add phone number in the header. On mobile, it closes the menu when clicking a link or the title.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const burgerMenuManagement = (function (){
+const burgerMenuManagement = (function () {
     const burger = document.querySelector('#burger-container');
     const menu = document.querySelector('#menu');
+    const title = document.querySelector('#title');
+    const links = document.querySelectorAll('#menu a');
 
     if (burger && menu) {
         burger.addEventListener('click', () => {
@@ -10,5 +12,30 @@ const burgerMenuManagement = (function (){
             menu.classList.toggle('active');
             document.body.classList.toggle('menu-open');
         });
+    }
+
+    if (links) {
+        links.forEach(link => {
+            link.addEventListener('click', () => {
+                shutDownMenu();
+            });
+        });
+    }
+
+    if (title) {
+        title.addEventListener('click', () => {
+            const menuOpen = document.body.classList.contains('menu-open');
+            if (menuOpen) {
+                shutDownMenu();
+            }
+        });
+    }
+
+    const shutDownMenu = () => {
+        if (burger && menu) {
+            burger.classList.replace('clicked', 'unclicked');
+            menu.classList.remove('active');
+            document.body.classList.remove('menu-open');
+        }
     }
 })();
